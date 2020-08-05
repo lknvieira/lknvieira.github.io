9d7a5a02be7f377699dcbe80f74a58ad3e587860
lknvieira <47356144+lknvieira@users.noreply.github.com>
2020-08-05 17:50:06 -0300

    New project

---
+++ precache-manifest.2cb2e0c7bcd3884c59f442b4b9753b3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b5abbd160d0ce6eb4fa0e08f182ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e408aaf1383fffa8f5c8",
+    "url": "/static/css/2.2885613b.chunk.css"
+  },
+  {
+    "revision": "e6ff1896973d32d78977",
+    "url": "/static/css/main.77d7872b.chunk.css"
+  },
+  {
+    "revision": "e408aaf1383fffa8f5c8",
+    "url": "/static/js/2.132f9352.chunk.js"
+  },
+  {
+    "revision": "0c46fb4d1bfb21f412f6f3a889dc0a75",
+    "url": "/static/js/2.132f9352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ff1896973d32d78977",
+    "url": "/static/js/main.ef491e10.chunk.js"
+  },
+  {
+    "revision": "b42d879a2671eb2a1913",
+    "url": "/static/js/runtime-main.8f555e62.js"
+  }
+]);
